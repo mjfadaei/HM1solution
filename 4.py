6f9087cc4a8673a8e3a1e65d43c6bc6fb6f90261
Mohammad Javad Fadaeieslam <mj.fadaei@gmail.com>
2021-12-04 12:48:20 +0330

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,14 @@
+str1 = 'semnan university'
+str2 = str1[::-1]
+print(str2)
+
+print('------------------')
+Str1='Semnan University' [::-1]
+print (Str1)
+
+print('------------------')
+srtl = "semnan university"
+x = ''
+for i in range(len(srtl)-1,-1,-1):
+        x += srtl[i]
+print(x)  
