84544c0ffa730ba2ae6e82066eb75ef6b458fe02
Mohammad Javad Fadaeieslam <mj.fadaei@gmail.com>
2021-12-04 12:48:39 +0330

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,38 @@
+n = int(input("please enter a number : "))
+for i in range(1,n+1):
+        print((i-1)*'_a_' + (n-i+1)* '*')
+print("----------------------")
+##My_num = int(5)
+##x = 0
+##for i in range(0, My_num):
+##    for j in range(0, x):
+##        print(end="-")
+##    x = x + 1
+##    for j in range(My_num, i, -1):
+##        print("*", end="")
+##    print("\r")
+##print("Done!")
+##print("-----------------------")
+##n=int(input("Enter n"))
+##for i in range(0,n):
+##    for j in range(0,n):
+##        if(i>j):
+##            print("-",end="")
+##        else:
+##            print("*",end="")
+##    print("\n")
+##print("-----------------------")
+##number = int(input("Enter a number : "))
+##for i in range(number):
+##    print("-" * i + "*" * number)
+##    number -=1
+##print("----------------------")
+##
+##n=int(input("enter number:"))
+##for x in range(n):
+##    for y in range(x):
+##        print("_",end="")
+##    for y in range(n-x):
+##        print("*",end="")  
+##    print()
+##pause()
